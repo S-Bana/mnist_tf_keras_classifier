380421068cb3b7342040c9e22a79da5af338d2cc
Sajad Bana Zadeh <sajadbanazadeh@gmail.com>
2025-07-06 00:16:25 +0330

    load dataset mnist

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -1,5 +1,10 @@
 # import libraries 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.utils import to_categorical
+
+
+# # load dataset
+(train_img, train_label), (test_img, test_label) = datasets.mnist.load_data()
