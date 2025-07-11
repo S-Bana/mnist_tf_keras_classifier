586b18f7320c6405394c83cb1b2123bd5889aa76
Sajad Bana Zadeh <sajadbanazadeh@gmail.com>
2025-07-11 17:22:55 +0330

    download dataset mnist, save it, load it from the local file

--- mnist_classifier.py
+++ mnist_classifier.py
@@ -6,5 +6,21 @@ from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.utils import to_categorical
 
 
-# # load dataset
-(train_img, train_label), (test_img, test_label) = datasets.mnist.load_data()
+# download dataset
+# (train_img, train_label), (test_img, test_label) = datasets.mnist.load_data()
+
+
+# # Save to a compressed .npz file
+# np.savez('mnist_local.npz', 
+#          train_img=train_img, 
+#          train_label=train_label, 
+#          test_img=test_img, 
+#          test_label=test_label)
+
+
+# Load from the local .npz file
+data = np.load('mnist_local.npz')
+train_img = data['train_img']
+train_label = data['train_label']
+test_img = data['test_img']
+test_label = data['test_label']
